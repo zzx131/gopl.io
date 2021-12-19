a153975e634c996721d5daf43333273762bec937
zzx <zzx_nuc@163.com>
2021-12-19 20:11:22 +0800

    添加时间

--- ch4/issues/main.go
+++ ch4/issues/main.go
@@ -23,8 +23,8 @@ func main() {
 	}
 	fmt.Printf("%d issues:\n", result.TotalCount)
 	for _, item := range result.Items {
-		fmt.Printf("#%-5d %9.9s %.55s\n",
-			item.Number, item.User.Login, item.Title)
+		fmt.Printf("#%-5d %9.9s %.55s %s\n",
+			item.Number, item.User.Login, item.Title, item.CreatedAt.Format("2006-01-02 15:04:05"))
 	}
 }
 
