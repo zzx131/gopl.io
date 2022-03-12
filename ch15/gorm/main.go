96f8343ae5751afe5106ab13c0b2d85a29ace6c8
zzx <zzx_nuc@163.com>
2022-03-12 19:25:05 +0800

    练习grom，多对多查询

---
+++ ch15/gorm/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+	"log"
+	"os"
+	"time"
+)
+
+type SysUser struct {
+	gorm.Model
+	Username string    `json:"username"`
+	RealName string    `json:"real_name" likeField:"real_name"`
+	Password string    `json:"password"`
+	OrgName  string    `json:"org_name"`
+	OrgId    *int64    `json:"org_id"`
+	LockFlag *int64    `json:"lock_flag"`
+	Roles    []SysRole `gorm:"many2many:sys_user_role;"`
+}
+
+type SysRole struct {
+	gorm.Model
+	Name        string `json:"name" likeField:"name"`
+	Code        string `json:"code"`
+	Remarks     string `json:"remarks"`
+	Description string `json:"description"`
+	IsEdit      bool   `json:"-"`
+}
+
+func main() {
+	dsn := "root:123456@tcp(127.0.0.1:3306)/tpf_knowledge?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), gormConfig())
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+	user := make([]SysUser, 0)
+	db.Preload("Roles").Find(&user)
+	fmt.Printf("%v\n", user)
+}
+
+// gormConfig 根据配置决定是否开启日志
+func gormConfig() *gorm.Config {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: 200 * time.Millisecond, // 慢 SQL 阈值
+			LogLevel:      logger.Info,            // Log level
+			Colorful:      true,                   // 禁用彩色打印
+		},
+	)
+	var config = &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true, // 使用单数表名
+		},
+		Logger: newLogger,
+	}
+	return config
+}
--- go.mod
+++ go.mod
@@ -10,19 +10,19 @@ require (
 	github.com/go-session/session v3.1.2+incompatible
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/glog v1.0.0
-	github.com/jessevdk/go-flags v1.5.0 // indirect
+	github.com/jessevdk/go-flags v1.5.0
 	github.com/jonboulle/clockwork v0.2.2 // indirect
 	github.com/kataras/iris/v12 v12.2.0-alpha5
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
 	github.com/lestrrat-go/strftime v1.0.5 // indirect
-	github.com/lithammer/fuzzysearch v1.1.3 // indirect
+	github.com/lithammer/fuzzysearch v1.1.3
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/tidwall/buntdb v1.2.9 // indirect
 	github.com/tidwall/gjson v1.13.0 // indirect
 	github.com/valyala/fasthttp v1.31.0 // indirect
 	github.com/xujiajun/nutsdb v0.6.0
 	go.uber.org/atomic v1.9.0 // indirect
-	go.uber.org/dig v1.13.0 // indirect
+	go.uber.org/dig v1.13.0
 	go.uber.org/multierr v1.7.0 // indirect
 	go.uber.org/zap v1.20.0
 	golang.org/x/net v0.0.0-20220114011407-0dd24b26b47d
@@ -30,7 +30,9 @@ require (
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
-	gopkg.in/ini.v1 v1.66.4 // indirect
+	gopkg.in/ini.v1 v1.66.4
 	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+	gorm.io/driver/mysql v1.3.2
+	gorm.io/gorm v1.23.2
 )
